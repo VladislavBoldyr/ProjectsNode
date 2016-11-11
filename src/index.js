e45e4ba4d8438c7ea90b24ae151db762b81670cc
Bolduryxa <vladiclav1996@mail.ru>
2016-11-11 19:16:58 +0600

    rewrite example task2B

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors());
 
  function canonize(fullName) {
    console.log(fullName.length);
    const localFullName = fullName.split(' ');
-   if (localFullName.length > 3) {
+   const number = /(0)|(1)|(2)|(3)|(4)|(5)|(6)|(7)|(8)|(9)/.test(fullName);
+   const defis = /(_)|(\/)/.test(fullName);
+
+   if ( localFullName.length > 3  || fullName.length == 0 || number == true || defis == true) {
      return 'Invalid fullname';
    }
    if (localFullName.length == 1) {
@@ -23,9 +27,7 @@ const app = express();
 app.get('/task2B',function (req,res) {
 
   const fullName = canonize(req.query.fullname.toString());
-  return res.json({
-    fullName,
-  });
+  return res.send(fullName);
 });
 
 app.listen(3000, function () {
